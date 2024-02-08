d6f3fc07fd0e3d7fac4cfa8f0139bbb40e99dd35
Ryan Helgeson <ryanhelgeson14@gmail.com>
2024-02-08 17:45:15 +0000

    Initial Commit

---
+++ weather/weatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherApp.swift
+//  weather
+//
+//  Created by Ryan Helgeson on 2/8/24.
+//
+
+import SwiftUI
+
+@main
+struct weatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
